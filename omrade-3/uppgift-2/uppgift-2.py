718f9529be30becbc6765c652aa58442db3bcf1a
Tomas Axelsson <tomas.axelsson89@gmail.com>
2026-08-24 10:04:24 +0200

    Dokumentera Område 3 uppgift 2

--- omrade-3/uppgift-2/uppgift-2.py
+++ omrade-3/uppgift-2/uppgift-2.py
@@ -1,4 +1,12 @@
+# Frågar användaren efter det första talet och omvandlar svaret till ett heltal
 tal1 = int(input("Skriv in ett tal: "))
+
+# Frågar användaren efter det andra talet och omvandlar svaret till ett heltal
+# Möjligt exekveringsfel: ValueError om användaren skriver text istället för ett heltal
 tal2 = int(input("Skriv in ett annat tal: "))
+
+# Adderar de två talen och sparar resultatet i variabeln summa
 summa = tal1 + tal2
-print(f"Summan av {tal1} och {tal2} är {summa}.")   
+
+# Skriver ut talen och resultatet för användaren
+print(f"Summan av {tal1} och {tal2} är {summa}.")
